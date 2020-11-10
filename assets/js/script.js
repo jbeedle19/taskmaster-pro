75f518566ece249a87c8bd877fa19ad70c042dc5
jbeedle19 <josh.beedle@gmail.com>
2020-11-10 11:51:32 -0500

    added persistence for each task step

--- assets/js/script.js
+++ assets/js/script.js
@@ -138,6 +138,56 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  /* activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  }, */
+  update: function(event) {
+    // Array to store the task data in
+    var tempArr = [];
+
+    // Loop over current set of children in sortable list
+    $(this).children().each(function(){
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // Add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      
+      });
+    });
+      // Trim down list's ID to match object property
+        var arrName = $(this)
+        .attr("id")
+        .replace("list-", "");
+
+      // Update array on tasks object and save
+        tasks[arrName] = tempArr;
+        saveTasks();
+  }
+});
 
 
 // modal was triggered
